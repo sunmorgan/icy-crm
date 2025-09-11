56c5b59fb22435deea5210a50a869cbbbc42802c
Morgan Sun <sunmorgan97@gmail.com>
2025-09-10 21:54:16 -0400

    deleted stuff and added navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-sky-200">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
+        <ol className="font-mono list-inside list-decimal text-lg text-center sm:text-left">
           Icy
         </ol>
+        <Link href="/dashboard" className="">
+          Dashboard
+        </Link>
       </main>
     </div>
   );
