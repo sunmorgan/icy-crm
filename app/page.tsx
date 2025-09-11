afd0555ed1908763b21f244cb60d909c5a7e0e7f
Morgan Sun <sunmorgan97@gmail.com>
2025-09-10 21:40:56 -0400

    Change name

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          Cold CRM
+          Icy
         </ol>
       </main>
     </div>
