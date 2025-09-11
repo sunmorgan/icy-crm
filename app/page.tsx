f908c691f89cdb13a71ed92c35105d7ac49b2d8f
Morgan Sun <sunmorgan97@gmail.com>
2025-09-11 14:29:49 -0400

    some new componenets

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-sky-200">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start text-stone-400">
         <ol className="font-mono list-inside list-decimal text-lg text-center sm:text-left">
           Icy
         </ol>
---
+++ components/dashboard/outreach.tsx
@@ -0,0 +1,3 @@
+export default function Outreach() {
+  return <>Your list of people ...</>;
+}
