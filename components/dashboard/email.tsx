8b8d277149b625c54d4635977539682c3a60b9e0
Morgan Sun <64807003+sunmorgan@users.noreply.github.com>
2025-12-29 15:54:05 -0400

    added functionality to send email content

--- components/dashboard/email.tsx
+++ components/dashboard/email.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from "react";
 
 interface EmailProp {
   name: string;
@@ -7,14 +8,28 @@ interface EmailProp {
 }
 
 export default function Email({ name, email, company }: EmailProp) {
-  console.log(name, email, company);
+  const [content, setContent] = useState("");
+
+  function sendEmail(emailContent: string) {
+    console.log(emailContent);
+  }
+
   return (
     <>
       <div>
         <textarea
           placeholder="Enter your email here"
           className="border border-s-stone-500"
+          onChange={(e) => setContent(e.target.value)}
         ></textarea>
+        <button
+          onClick={() => {
+            sendEmail(content);
+          }}
+          className="border border-s-stone-700"
+        >
+          Send
+        </button>
       </div>
     </>
   );
